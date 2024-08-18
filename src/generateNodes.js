e2607dcbb4cdd0840836c60132ab6c739c3afe23
realrobertjohnson <127041638+realrobertjohnson@users.noreply.github.com>
2024-08-18 14:38:44 -0600

    test

--- src/generateNodes.js
+++ src/generateNodes.js
@@ -2,8 +2,8 @@ import { COLOR_MAP } from "./constants";
 const { board } = window.miro;
 const generateCardObjectFor = (object, x, y) => {
   let cardColor = "#2399f3";
-  
-  if(object?.style?.fillColor) {
+
+  if (object?.style?.fillColor) {
     const objectFillColor = object.style.fillColor;
     if (objectFillColor !== "transparent") {
       if (COLOR_MAP[objectFillColor]) {
@@ -41,37 +41,37 @@ export const generateNodes = async () => {
     generateCardObjectFor(item, item.x + 800, item.y)
   );
 
-// START MAX CODE
-  
-// Single out the first selected item
-let selectedItem
-if (selectedWidgets.length > 0){selectedItem = selectedWidgets[0]}
- 
-// Get the content from the first item selected in the array of selected items -- this is '.content' for all items except cards, whose primary info is stored in '.title'
-let itemContent=""
-if (selectedItem.type === "card"){itemContent = selectedItem.title}else{itemContent=selectedItem.content}
+  // START MAX CODE
 
-// Create mind map root node -- insert 'itemContent' into content and adjust the 
-const root = await board.experimental.createMindmapNode({
-nodeView: {
-content: `${itemContent}`,
-  },
-    x: selectedItem.x+800,
+  // Single out the first selected item
+  let selectedItem;
+  if (selectedWidgets.length > 0) { selectedItem = selectedWidgets[0] }
+
+  // Get the content from the first item selected in the array of selected items -- this is '.content' for all items except cards, whose primary info is stored in '.title'
+  let itemContent = "";
+  if (selectedItem.type === "card") { itemContent = selectedItem.title } else { itemContent = selectedItem.content }
+
+  // Create mind map root node -- insert 'itemContent' into content and adjust the 
+  const root = await board.experimental.createMindmapNode({
+    nodeView: {
+      content: `${itemContent}`,
+    },
+    x: selectedItem.x + 800,
     y: selectedItem.y,
- }); 
-//END MAX CODE
- 
-  
-//  const cardsGeneratedPromise = cardsObjects.map(async (card) => {
-//    const cardResult = board.createCard(card);
-//    return cardResult;
-//  });
+  });
+  //END MAX CODE
 
-// commented out
-//  const cardsGenerated = await Promise.all(cardsGeneratedPromise);
-//  const cardCount = cardsGenerated.length;
-//  if (cardCount > 0) {
-//    await board.viewport.zoomTo(cardsGenerated);
-//    console.log('Cardsy generated ${cardCount} cards for you.');
-//  }
+
+  //  const cardsGeneratedPromise = cardsObjects.map(async (card) => {
+  //    const cardResult = board.createCard(card);
+  //    return cardResult;
+  //  });
+
+  // commented out
+  //  const cardsGenerated = await Promise.all(cardsGeneratedPromise);
+  //  const cardCount = cardsGenerated.length;
+  //  if (cardCount > 0) {
+  //    await board.viewport.zoomTo(cardsGenerated);
+  //    console.log('Cardsy generated ${cardCount} cards for you.');
+  //  }
 };
